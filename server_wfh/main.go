8ef0d12717e7e1de3dc85df4ab42b2a547f23ca4
Ingrid Ragnarson <ingrirag@stud.ntnu.no>
2022-01-26 10:27:55 +0100

    update exercise 2

---
+++ server_wfh/main.go
@@ -0,0 +1,11 @@
+package main
+
+
+func main(){
+	
+
+
+}
+
+//send, listen, dial, 
+//
--- server_wfh/our_server_TCPC.go
+++
@@ -1,9 +0,0 @@
-package main
-
-addr = new InternetAddress(serverIP, serverPort) 
-sock = new Socket(tcp) // TCP, aka SOCK_STREAM
-sock.connect(addr)
-// use sock.recv() and sock.send()
-
-//server_ip = 30000
-
