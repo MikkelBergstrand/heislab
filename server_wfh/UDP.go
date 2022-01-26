98fc69ce810ce50981ed87f8b6097f2bc90ca515
Ingrid Ragnarson <ingrirag@stud.ntnu.no>
2022-01-26 10:53:25 +0100

    go Peder

--- server_wfh/UDP.go
+++ server_wfh/UDP.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func udp_client(l_addr string, port string) {
+	p := make([]byte, 2048)
+	update_address := l_addr + ":" + port
+	conn, err := net.Dial("udp", update_address)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+	_, err = bufio.NewReader(conn).Read(p)
+	if err == nil {
+		fmt.Printf("%s\n", p)
+	} else {
+		fmt.Printf("Some error %v\n", err)
+	}
+	conn.Close()
+
+}
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
+	if err != nil {
+		fmt.Printf("Couldn't send response %v", err)
+	}
+}
+
+func udp_server(l_addr string) {
+	p := make([]byte, 2048)
+	addr := net.UDPAddr{
+		Port: 1234,
+		IP:   net.ParseIP(l_addr),
+	}
+	ser, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return
+	}
+	for {
+		_, remoteaddr, err := ser.ReadFromUDP(p)
+		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+		if err != nil {
+			fmt.Printf("Some error  %v", err)
+			continue
+		}
+		go sendResponse(ser, remoteaddr)
+	}
+}
--- server_wfh/main.go
+++ server_wfh/main.go
@@ -1,8 +1,12 @@
 package main
 
+import(
+
+)
 
 func main(){
-	
+	l_addr := "127.0.0.1" //"127.0.0.1:1234"
+	port:= 30000
 
 
 }
